c6a79f9b6e16fdecb523091533badf5c0b3e8ac8
Simeon-Azeh <144020513+Simeon-Azeh@users.noreply.github.com>
2024-07-10 12:13:47 +0200

    done

--- src/Components/CardItem.jsx
+++ src/Components/CardItem.jsx
@@ -1,9 +1,12 @@
 import React from 'react'
 
-function CardItem({ title, description }) {
+function CardItem({ title, description, Icon }) {
   return (
     <div className=''>
         <div className=''>
+         <div>
+            <Icon />
+            </div>   
         <h2 className='text-red-900'>{title}</h2>
         <p>{description}</p>
         </div>
