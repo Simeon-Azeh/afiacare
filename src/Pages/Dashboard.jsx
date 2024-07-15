1d9884cd6ebfe029fb91d573e51952d610c100cc
Simeon-Azeh <144020513+Simeon-Azeh@users.noreply.github.com>
2024-07-15 23:44:19 +0200

    updates to dashboard

--- src/Pages/Dashboard.jsx
+++ src/Pages/Dashboard.jsx
@@ -13,8 +13,93 @@ import { SiFigshare } from "react-icons/si";
 import BookingDashboard from '../Components/BookingDashBoard';
 import BloodGroupCard from '../Components/BloodGroupCard';
 import BMICard from '../Components/BMICard';
+import MedReportDashboard from '../Components/MedReportDashboard';
+import { Calendar, theme, Badge } from 'antd';
+import AppointmentDis from '../Components/AppointmentDis';
+import RecentActivities from '../Components/RecentActivities';
+
+
+
 
 function Dashboard() {
+  const { token } = theme.useToken();
+  const wrapperStyle = {
+    borderRadius: token.borderRadiusLG,
+  };
+  const onPanelChange = (value, mode) => {
+    console.log(value.format('YYYY-MM-DD'), mode);
+  };
+
+  //Event details
+
+  const getListData = (value) => {
+    let listData = []; // Specify the type of listData
+    switch (value.date()) {
+      case 1:
+        listData = [
+          {
+            type: 'warning',
+            content: 'Appt',
+          },
+        
+        ];
+        break;
+      case 10:
+        listData = [
+          {
+            type: 'warning',
+            content: 'App',
+          },
+         
+        ];
+        break;
+      case 15:
+        listData = [
+          {
+            type: 'success',
+            content: 'event',
+          },
+        
+        ];
+        break;
+      default:
+    }
+    return listData || [];
+  };
+  const getMonthData = (value) => {
+    if (value.month() === 1) {
+      return 1394;
+    }
+  };
+
+  //Event rendering 
+  const monthCellRender = (value) => {
+    const num = getMonthData(value);
+    return num ? (
+      <div className="notes-month">
+        <section>{num}</section>
+        <span>Event</span>
+      </div>
+    ) : null;
+  };
+  const dateCellRender = (value) => {
+    const listData = getListData(value);
+    return (
+      <ul className="events">
+        {listData.map((item) => (
+          <li key={item.content}>
+            <Badge status={item.type} text={item.content} />
+          </li>
+        ))}
+      </ul>
+    );
+  };
+  const cellRender = (current, info) => {
+    if (info.type === 'date') return dateCellRender(current);
+    if (info.type === 'month') return monthCellRender(current);
+    return info.originNode;
+  };
+
   return (
     <div className="flex h-screen">
       <div>
@@ -46,7 +131,16 @@ function Dashboard() {
        
       <div className="flex flex-col px-4 md:flex-row">
          <div className='md:w-[50%]'>
+          <div>
           <BookingDashboard />
+          </div>
+          
+         <div style={wrapperStyle} className='p-4' >
+         <Calendar fullscreen={false} onPanelChange={onPanelChange} cellRender={cellRender} />
+         </div>
+         <div>
+         <AppointmentDis />
+         </div>
          </div>
          <div className='md:w-[50%]'>
           <div>
@@ -55,7 +149,12 @@ function Dashboard() {
         <div>
           <BMICard />
         </div>
-   
+          <div className='mt-2'>
+            <MedReportDashboard />
+          </div>
+          <div>
+            <RecentActivities />
+          </div>
           </div>
       </div>
       </div>
